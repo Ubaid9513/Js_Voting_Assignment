49377c3615d3dbe1a8ec41fda7861bb949cb86eb
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-08-27 01:53:59 +0500

    updated

---
+++ app.js
@@ -0,0 +1,29 @@
+// var input = document.getElementById("input");
+// var names = document.getElementsByClassName("names");
+// var serialNo = document.getElementsByClassName("serialNo");
+// var votes = document.getElementsByClassName("votes");
+// var num = 0;
+// var count = 0;
+
+// console.log(names);
+
+
+// function add() {
+//   if (input.value === "") {
+//     alert("Enter Particiant Name");
+//   } else {
+//     // Create li for names list
+//     var li = document.createElement("li");
+//     li.innerHTML += input.value;
+//     names.appendChild(li);
+//     input.value = "";
+//     // Create votes list
+//     li.setAttribute("onclick", "addVote()");
+//   }
+// }
+// function addVote() {
+//   count++;
+//   var votesP = document.createElement("p");
+//   votes.appendChild(votesP);
+//   votesP.innerHTML += count;
+// }
